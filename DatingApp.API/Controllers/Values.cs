583d79225c1b736be087782cabbebf01234649e2
Joseph Lee <josephlee0307@hotmail.com>
2020-05-16 18:03:49 -0500

    Initial Commit

---
+++ DatingApp.API/Controllers/Values.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Values : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public Values(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+            if (value == null)
+                return NotFound();
+            
+            return Ok(value);
+        }
+    }
+}
