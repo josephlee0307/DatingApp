74a68c4e2b1b8b8dfd551b8ff7d717a61ebb4ef8
Joseph Lee <josephlee0307@hotmail.com>
2020-05-17 22:10:54 -0500

    End of Section 3

--- DatingApp.API/Controllers/Values.cs
+++ DatingApp.API/Controllers/Values.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class Values : ControllerBase
@@ -28,6 +30,7 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(
+            8, 
+            MinimumLength = 4, 
+            ErrorMessage = "The password should be 4 to 8 characters.")]
+        public string Password { get; set; }
+    }
+}
